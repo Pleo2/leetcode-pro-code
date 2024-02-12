c0a2f6ab16fe2d4aa30a7d04240d150b9733fea9
pleo2 <leooel23m@gmail.com>
2024-02-12 09:02:01 -0400

    add: isRowItemRepeat and isColumnRepeat in progress

--- src/array-&-hashing/ValidSudoku.ts
+++ src/array-&-hashing/ValidSudoku.ts
@@ -8,9 +8,59 @@ Note:
 
 A Sudoku board (partially filled) could be valid but is not necessarily solvable.
 Only the filled cells need to be validated according to the mentioned rules.
+
+[
+  ["5","3",".",".","7",".",".",".","."] // veryfy if the numbers is unic
+,["6",".",".","1","9","5",".",".","."] // push the 
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]
+]
+
  */
 
-export default function isValidSudoku(board: string[][]): unknown {
-    // Verify is row must contain the digits 1-9 witouth repetition
-   
+export default function isValidSudoku(board: string[][]): any {
+    isRowItemRepeat(board)
+    // map  colums of the array
+
+    const arrayAgrupado = board.map((row, idxRow) => {
+        return row.map((item, idxColumn) => {
+            return {
+                idxColumn,
+                idxRow,
+                item,
+            }
+        })
+    })
+
+    const columnArr = new Array(9).fill([])
+
+    for (let i = 0; i < arrayAgrupado.length; i++) {
+        const element = arrayAgrupado[i];
+
+        for (let index = 0; index < element.length; index++) {
+            const item = element[index];
+            if(item.idxColumn === index) {
+                columnArr[i].push(item.item)
+            }
+        }
+    }
+    
+    console.log(columnArr)
+}
+
+function isRowItemRepeat(board: string[][]): boolean | string {
+    for (let idx = 0; idx < board.length; idx++) {
+        const element = board[idx]
+        const row = element.filter(item => item !== '.')
+        const set = new Set(row)
+        if (set.size !== row.length) {
+            return false
+        }
+    }
+    return 'no repeat'
 }
